8f150da8a3f3ed2103cc55bba828d81882ae04ba
Matt <matt@randomforestweb.com>
2024-01-23 04:50:57 +0900

    pwa

---
+++ sw.js
@@ -0,0 +1,58 @@
+const swVersion = 'v1';
+const cacheName = `oft-app-${swVersion}`;
+
+const appStaticResources = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/app.js',
+    '/manifest/manifest.json',
+    '/favicon/favicon.ico'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            cache.addAll(appStaticResources);
+        })()
+    );
+});
+
+// delete old caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if(name !== cacheName) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+            await clients.claim();
+        })()
+    );
+});
+
+// intercept server requests and respond with cached responses instead of going to network
+self.addEventListener('fetch', (e) => {
+    // direct app to always go to homepage
+    if(e.request.mode === 'navigate') {
+        e.respondWith(caches.match('/'));
+        return;
+    }
+
+    // for all other requests, go to the cache first, then the network
+    e.respondWith(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            const cachedResponse = await cache.match(e.request.url);
+            if(cachedResponse) {
+                return cachedResponse;
+            }
+            return new Response(null, {status: 404});
+        })(),
+    );
+});
